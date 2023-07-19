1e14e7f68c99b122bf7c60b36cf38b2194d3cdbb
davi_mendoca <davi.p.m94@gmail.com>
2023-07-19 14:54:56 -0300

    update styles

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,21 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
-import { Button, Navbar, Nav, Container } from "react-bootstrap";
+import { Button, Navbar, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+
+import { NavbarBrandGold, CustomNav } from "./styles.ts";
 
 export default function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="/" style={{ color: "gold" }}>
+        <NavbarBrandGold href="/">
           <FontAwesomeIcon icon={faVideoSlash} /> Gold
-        </Navbar.Brand>
+        </NavbarBrandGold>
         <Navbar.Toggle aria-controls="navbarControll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }}>
+          <CustomNav className="me-auto my-2 my-lg-0">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/watchlist">Watch List</NavLink>
-          </Nav>
+          </CustomNav>
           <Button variant="outline-info" className="me-2">
             Login
           </Button>
---
+++ frontend/src/components/header/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Nav, Navbar } from "react-bootstrap";
+
+export const NavbarBrandGold = styled(Navbar.Brand)`
+  color: gold;
+`;
+
+export const CustomNav = styled(Nav)`
+  max-height: 100px;
+`;
