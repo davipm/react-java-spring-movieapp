fc53dd978b8f6ae72073c8f97ba15899988defec
davi_mendoca <davi.p.m94@gmail.com>
2023-07-18 23:03:15 -0300

    create review controller

--- src/main/java/dev/davi/movies/Review.java
+++ src/main/java/dev/davi/movies/Review.java
@@ -4,15 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Document(collection = "review")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
-    @Id
     private ObjectId id;
     private String body;
+    private LocalDateTime created;
+    private LocalDateTime update;
+
+    public Review(String body, LocalDateTime created, LocalDateTime update) {
+        this.body = body;
+        this.created = created;
+        this.update = update;
+    }
 }
