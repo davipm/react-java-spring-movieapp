01af889703b608bf7754e469b02f6004486290dd
davi_mendoca <davi.p.m94@gmail.com>
2023-07-22 01:19:42 -0300

    adding stylelint

--- frontend/src/components/hero/styles.ts
+++ frontend/src/components/hero/styles.ts
@@ -12,16 +12,16 @@ export const MovieCardContainer = styled.div`
 `;
 
 export const MovieCard = styled.div<{ img: string }>`
-  background-repeat: no-repeat;
   width: 100%;
   height: 100%;
-  background-size: cover;
   background-image: linear-gradient(
       to bottom,
-      rgba(0, 0, 0, 0),
-      rgba(0, 0, 0, 1)
+      rgb(0 0 0 / 0%),
+      rgb(0 0 0 / 100%)
     ),
     url("${(p) => p.img}");
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const MovieDetails = styled.div`
@@ -31,18 +31,18 @@ export const MovieDetails = styled.div`
   justify-content: space-evenly;
   width: 100%;
 
-  @media (max-width: 800px) {
+  @media (width <= 800px) {
+    top: 20px;
     flex-direction: column;
     align-items: center;
-    top: 20px;
   }
 `;
 
 export const MoviePoster = styled.div`
   height: 300px;
+  overflow: hidden;
   border: 1px solid gold;
   border-radius: 10px;
-  overflow: hidden;
 
   > img {
     width: 100%;
@@ -51,31 +51,31 @@ export const MoviePoster = styled.div`
 `;
 
 export const MovieTitle = styled.div`
-  color: white;
   display: flex;
   align-items: center;
+  color: white;
 `;
 
 export const MovieButtonContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
   width: 300px;
 `;
 
 export const PlayButtonIconContainer = styled.div`
+  width: 150px;
   padding: 0;
   margin: 0;
-  width: 150px;
 `;
 
 export const PLayButtonIcon = styled(FontAwesomeIcon)`
   padding: 0;
   margin: 0;
-  color: gold;
   font-size: 3rem;
-  transition: 0.3s;
+  color: gold;
   cursor: pointer;
+  transition: 0.3s;
 
   :hover {
     font-size: 4rem;
--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -8,13 +8,20 @@ export const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
+    font-family: "Noto Sans", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: var(--bs-black);
+    text-align: left;
+    background-color: var(--bs-white);
   }
   
   .app {
     display: flex;
     flex-direction: column;
     min-height: 100vh;
-    background-color: black;
-    color: white;
+    color: var(--bs-white);
+    background-color: var(--bs-black);
   }
 `;
